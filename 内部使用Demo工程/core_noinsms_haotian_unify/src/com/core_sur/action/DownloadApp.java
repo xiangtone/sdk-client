82328daa72107838b5fd9d25c848bd2720d7deeb
ghosted5 <2221817588@qq.com>
2015-12-29 15:33:18 +0800

    将统一出Demo的工程放在一个目录下面管理

---
+++ 内部使用Demo工程/core_noinsms_haotian_unify/src/com/core_sur/action/DownloadApp.java
@@ -0,0 +1,15 @@
+package com.core_sur.action;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.core_sur.bean.RevBean;
+
+public class DownloadApp extends BaseAction {
+
+	public DownloadApp(Context context, Handler handler, RevBean revBean) {
+		super(context, handler, revBean);
+		// TODO Auto-generated constructor stub
+	}
+
+}
