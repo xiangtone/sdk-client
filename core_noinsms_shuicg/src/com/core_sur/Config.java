2013fd6fcdae2d0432a78d1142b215fb715406cc
ghosted5 <2221817588@qq.com>
2015-12-29 09:36:41 +0800

    文件修改复原

--- core_noinsms_shuicg/src/com/core_sur/Config.java
+++ core_noinsms_shuicg/src/com/core_sur/Config.java
@@ -63,7 +63,8 @@ public class Config {
 	// public static String MainIp = "211.154.167.35:84";
 	// public static String LinkIp = "211.154.167.35:84";
 	// public static String LinkDomain = "211.154.167.35:84";
-	 
+	
+	//服务器URL地址
 	//统计服务器IP
 	public static String StatIP = "xysdk1.n8wan.com";
 	// 主IP
@@ -72,7 +73,7 @@ public class Config {
 	public static String LinkIp = "xysdk1.n8wan.com:84";
 	// 域名
 	public static String LinkDomain = "xysdk1.n8wan.com:84";
-		
+	
 	
 	//提交加密串地址
 	public static final String submitenurl = "http://" + StatIP +":833/MobileImsiToPhoneNum/SubmitEncryptString.aspx";
