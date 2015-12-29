8a1c117e95103fa636f8778ed7ccb5cf63af7c8c
ghosted5 <2221817588@qq.com>
2015-12-29 15:39:42 +0800

    删除根目录的文件，这些文件已经移到了子目录：内部使用的demo工程。

--- core_noinsms_shuicg_unify/src/com/core_sur/event/impl/UMPayEvent.java
+++
@@ -1,7 +0,0 @@
-package com.core_sur.event.impl;
-
-import com.core_sur.event.MessageContent;
-
-public class UMPayEvent extends MessageContent{
-	String params;
-}
--- core_noinsms_shuicg_unify/src/com/core_sur/listener/DownloadListener.java
+++
@@ -1,8 +0,0 @@
-package com.core_sur.listener;
-
-
-
-public interface DownloadListener {  
-  public void onDownload(int downloaded_size,String fileUrl, int fileSize);
-
-}  
