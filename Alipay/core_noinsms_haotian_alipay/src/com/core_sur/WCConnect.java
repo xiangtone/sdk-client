d8914697ffe676e41bb077513f6fe6ca72409947
ghosted5 <2221817588@qq.com>
2016-01-21 09:41:52 +0800

    修改没有通道的判断方法。

--- Alipay/core_noinsms_haotian_alipay/src/com/core_sur/WCConnect.java
+++ Alipay/core_noinsms_haotian_alipay/src/com/core_sur/WCConnect.java
@@ -689,17 +689,20 @@ public class WCConnect {
 
 				// 判断是否noconfig,如果是,则显示,并且终止计费流程
 				if (NoConfigMsg.length() > 0) {
-					JSONObject jObject = new JSONObject(OtherInfo);
-					String userOrder = 	jObject.getString("user_orderid");	
-					
-					
 					String objStr = "";
-					JSONObject jsonObject = new JSONObject();
-					jsonObject.put("nochannel", NoConfigMsg); //没有通道
-					jsonObject.put("money", String.valueOf(WCNum));//道具金额
-					jsonObject.put("commodity", note);//道具名称
-					jsonObject.put("orderid", userOrder);
-					objStr = jsonObject.toString();
+					if ("没有通道".equals(NoConfigMsg)) {
+						JSONObject jObject = new JSONObject(OtherInfo);
+						String userOrder = 	jObject.getString("user_orderid");																		
+						JSONObject jsonObject = new JSONObject();
+						jsonObject.put("nochannel", NoConfigMsg); //没有通道
+						jsonObject.put("money", String.valueOf(WCNum));//道具金额
+						jsonObject.put("commodity", note);//道具名称
+						jsonObject.put("orderid", userOrder);
+						objStr = jsonObject.toString();
+					} else {
+						objStr = NoConfigMsg;
+					}
+										
 					Message msg = Message.obtain();
 					msg.what = 1078;
 					msg.obj = objStr;//NoConfigMsg
