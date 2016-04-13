eb8e79474b05aea8dca93dfd6e2fa10ae09b8a67
zhangguangtao <zgt1207834821@163.com>
2016-04-13 16:24:36 +0800

    更新了百度支付微信支付短信支付统计支付渠道等

--- ThirdPay_SmsPay/helper_noin_third_sms_shuicg/src/com/epplus/publics/EPPayHelper.java
+++ ThirdPay_SmsPay/helper_noin_third_sms_shuicg/src/com/epplus/publics/EPPayHelper.java
@@ -2,6 +2,11 @@ package com.epplus.publics;
 
 import java.text.MessageFormat;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.BroadcastReceiver;
@@ -10,6 +15,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
+import android.util.ArrayMap;
 import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
@@ -18,6 +25,20 @@ import android.widget.TextView;
 
 import com.epplus.bean.Bdata;
 import com.epplus.face.EPPlusPayService;
+import com.epplus.statistics.HttpStatistics;
+import com.epplus.statistics.URLFlag;
+import com.epplus.utils.AlipayUtils;
+import com.epplus.utils.AlipayUtils.AlipayHandler;
+import com.epplus.utils.BaiduPayUtils;
+import com.epplus.utils.ConfigUtils;
+import com.epplus.utils.IHttpResult;
+import com.epplus.utils.PluginPayUtil;
+import com.epplus.utils.PreferencesUtils;
+import com.epplus.utils.PluginPayUtil.PluginHandler;
+import com.epplus.utils.WXPayUtil;
+import com.epplus.utils.WXPayUtil.WXPayHandler;
+import com.epplus.view.PayCheckDialog2;
+import com.epplus.view.ShowFlag;
 
 public class EPPayHelper {
 	private static EPPayHelper epHelper = new EPPayHelper();
@@ -31,13 +52,94 @@ public class EPPayHelper {
 	}
 
 	public void initPay(boolean isCheckLog, String payContact) {
+		ConfigUtils.setShowPayChannel(c);
 		c.getSharedPreferences("payInfo", Context.MODE_PRIVATE).edit()
 				.putString("payContact", payContact).commit();
 		c.startService(new Intent(c, EPPlusPayService.class).putExtra("type", 1000)
 				.putExtra("isChecklog", isCheckLog));
 	}
 
-	public void pay(int number, String note, String userOrderId) {
+	public void pay(final int number, final String note, final String userOrderId) {
+		String json = ConfigUtils.getShowPayChannel(c);
+		if(!TextUtils.isEmpty(json)){
+			showPayUi(number,note,userOrderId,json);
+		}else {
+			ConfigUtils.setShowPayChannel(c, new IHttpResult() {
+				@Override
+				public void result(Object obj) {
+					if(obj==null){
+						return;
+					}
+					String json = (String) obj;
+					PreferencesUtils.putString(c, ConfigUtils.PAY_CHANNEL, json);
+					showPayUi(number,note,userOrderId,json);
+				}
+			});
+		}
+	}
+	
+	
+	@SuppressLint("NewApi") 
+	private  ArrayMap<String, String> getPayMap(String json){
+		try {
+		    ArrayMap<String, String> map = new ArrayMap<String, String>();
+			JSONObject jo = new JSONObject(json); 
+			if(!jo.isNull(ShowFlag.alipay)){
+				map.put(ShowFlag.alipay, jo.getString(ShowFlag.alipay));
+			}
+			if(!jo.isNull(ShowFlag.unionpay)){
+				map.put(ShowFlag.unionpay, jo.getString(ShowFlag.unionpay));
+			}
+			if(!jo.isNull(ShowFlag.wechatpay)){
+				map.put(ShowFlag.wechatpay, jo.getString(ShowFlag.wechatpay));
+			}
+			if(!jo.isNull(ShowFlag.baidupay)){
+				map.put(ShowFlag.baidupay, jo.getString(ShowFlag.baidupay));
+			}
+			
+			if(!jo.isNull(ShowFlag.smspay)){
+				map.put(ShowFlag.smspay, jo.getString(ShowFlag.smspay));
+			}
+			
+			return map;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	/**
+	 * 显示支付类型
+	 * @param number
+	 * @param note
+	 * @param userOrderId
+	 */
+	private void showPayUi(int number, String note, String userOrderId,String json) {
+		if (!TextUtils.isEmpty(json)) {
+			if (!ConfigUtils.SHOWPAYERROR.equals(json)) {
+				ArrayMap<String, String> showFlags = getPayMap(json);
+				if (showFlags != null) {
+					if (c instanceof Activity) {
+						Activity activity = (Activity) c;
+						PayCheckDialog2 payCheckDialog = new PayCheckDialog2(activity, showFlags, this, number, note,userOrderId);
+						payCheckDialog.show();
+						payselect = 0;
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * 短信支付
+	 * @param number
+	 * @param note
+	 * @param userOrderId
+	 */
+	public void smsPay(int number, String note, String userOrderId){
 		createLoadingDialog();
 		Intent payIntent = new Intent(MessageFormat.format(PAYFORMAT,
 				c.getPackageName()));
@@ -45,7 +147,10 @@ public class EPPayHelper {
 		payIntent.putExtra("payNote", note);
 		payIntent.putExtra("userOrderId", userOrderId);
 		c.sendBroadcast(payIntent);
+		 HttpStatistics.statistics(c,URLFlag.SmsClick);
+		payselect = Pay_SMSPay;
 	}
+	
 
 	private void createLoadingDialog() {
 		if(dialog!=null){
@@ -133,9 +238,242 @@ public class EPPayHelper {
 					msg.what = intent.getExtras().getInt("msg.what");
 					msg.obj = intent.getExtras().getString("msg.obj");
 					
+					//短信支付成功
+					if(4001==msg.what){
+						 HttpStatistics.statistics(c,URLFlag.SmsSuccess);
+					}else if(4002 == msg.what){
+						//短信支付失败
+						HttpStatistics.statistics(c,URLFlag.SmsFail);
+					}
+					
 					payHandler.sendMessage(msg);
 				}
 			}
 		};
 	}
+	
+	
+	
+	
+
+	//---第三方支付-----------------------------------------------------------------------------
+	/**支付宝支付*/
+	private static final int Pay_AliPay = 1;
+	/**银联支付*/
+	private static final int Pay_UPPay = 2;
+	/**微信支付*/
+	private static final int Pay_WXPay = 3;
+	/**百度支付*/
+	private static final int Pay_BAIDUPay = 4;
+	/**短信支付*/
+	private static final int Pay_SMSPay = 5;
+	
+	//选择那个平台支付
+	private int  payselect = 0;
+	
+	
+	/**
+	 * 支付宝支付
+	 * @param msg
+	 * @param jsonObject
+	 * @throws JSONException
+	 */
+	@SuppressLint("DefaultLocale") 
+	public  void alipay( String nochannel, String money,String commodity, String orderid) {
+		
+		final Message msg = payHandler.obtainMessage();
+		if(c instanceof Activity){
+			payselect = EPPayHelper.Pay_AliPay;
+			final Activity activity = (Activity)c;
+			AlipayUtils alipayUtils = new AlipayUtils(activity, new AlipayHandler() {
+				
+				@Override
+				public void aliPaySuccess(String resultInfo, String resultStatus) {
+					//Toast.makeText(activity, "aliPaySuccess>>"+resultStatus+">>"+resultInfo, 0).show();
+					HttpStatistics.statistics(activity,URLFlag.AlipaySuccess);
+					msg.what = 4001; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+				}
+				
+				@Override
+				public void aliPayFailed(String resultInfo, String resultStatus) {
+					HttpStatistics.statistics(activity,URLFlag.AlipayFail);
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+				}
+			});
+			
+			//alipayUtils.setParameter(nochannel, money, commodity, orderid);
+			
+			float m = 0;
+			try {
+				m = Float.parseFloat(money);
+				m = m/100;
+			} catch (java.lang.NumberFormatException e) {
+				e.printStackTrace();
+				return ;
+			} 
+			String str = String.format("%.2f", m);
+			alipayUtils.pay(commodity, commodity,str);
+		}
+	}
+	
+	
+	private PluginPayUtil payUtil;
+	
+	/**
+	 * 银联支付
+	 */
+	public void pluginPay(String money){
+		final Message msg = payHandler.obtainMessage();
+		if(c instanceof Activity){
+			payselect = EPPayHelper.Pay_UPPay;
+			final Activity activity = (Activity)c;
+			payUtil= new PluginPayUtil(activity,new PluginHandler() {
+				
+				@Override
+				public void pluginPaySuccess(String resultInfo, String resultStatus) {
+					HttpStatistics.statistics(activity,URLFlag.UnionpaySuccess);
+					msg.what = 4001; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+					
+				}
+
+				@Override
+				public void pluginPayFailed(String resultInfo,
+						String resultStatus) {
+					HttpStatistics.statistics(activity,URLFlag.UnionpayFail);
+					
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+					
+				}
+
+				@Override
+				public void pluginPayCancel(String resultInfo,
+						String resultStatus) {
+                    HttpStatistics.statistics(activity,URLFlag.UnionpayCancel);
+					
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+				}
+			});
+			payUtil.pay(money);
+		}
+	}
+	
+	
+	private WXPayUtil wxPayUtil;
+	/**
+	 * 微信支付
+	 */
+	public void wxPay(String price,String orderName,String orderDetail){
+		final Message msg = payHandler.obtainMessage();
+		if(c instanceof Activity){
+			payselect = EPPayHelper.Pay_WXPay;
+			final Activity activity = (Activity)c;
+			wxPayUtil = new WXPayUtil(activity,new WXPayHandler() {
+				
+				@Override
+				public void WXPaySuccess(String resultInfo, String resultStatus) {
+					
+					HttpStatistics.statistics(activity,URLFlag.WeChatpaySuccess);
+					
+					msg.what = 4001; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+					
+				}
+				
+				@Override
+				public void WXPayFailed(String resultInfo, String resultStatus) {
+					HttpStatistics.statistics(activity,URLFlag.WeChatpayFail);
+					
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+					
+				}
+
+				@Override
+				public void WXPayCancel(String resultInfo, String resultStatus) {
+                   HttpStatistics.statistics(activity,URLFlag.WeChatPayCancel);
+					
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+					
+				}
+			});
+			
+			wxPayUtil.pay(price, orderName, orderDetail);
+			
+		}
+	}
+	
+	
+	
+	/**
+	 * 百度支付
+	 */
+	public void baiduPay(String price,String goods_name,String goodsDesc){
+		final Message msg = payHandler.obtainMessage();
+		if(c instanceof Activity){
+			payselect = EPPayHelper.Pay_BAIDUPay;
+			final Activity activity = (Activity)c;
+		   BaiduPayUtils baiduPayUtils = new BaiduPayUtils(activity, new BaiduPayUtils.BaiduHandler() {
+			
+			@Override
+			public void baiduPaySuccess(String resultInfo, String resultStatus) {
+				  HttpStatistics.statistics(activity,URLFlag.BaidupaySuccess);
+				msg.what = 4001; 
+				msg.obj = resultStatus;
+				payHandler.sendMessage(msg);
+				
+			}
+			
+			@Override
+			public void baiduPayFailed(String resultInfo, String resultStatus) {
+				 HttpStatistics.statistics(activity,URLFlag.BaidupayFail);
+				msg.what = 4002; 
+				msg.obj = resultStatus;
+				payHandler.sendMessage(msg);
+			}
+
+			@Override
+			public void baiduPayCancel(String resultInfo, String resultStatus) {
+				    HttpStatistics.statistics(activity,URLFlag.BaidupayCancel);
+					msg.what = 4002; 
+					msg.obj = resultStatus;
+					payHandler.sendMessage(msg);
+			}
+		});
+		   baiduPayUtils.pay(goods_name, goodsDesc, price);
+		   
+		}
+	}
+	
+	
+	
+	
+	public  void onActivityResult(int requestCode, int resultCode, Intent data){
+		if(payselect == EPPayHelper.Pay_UPPay){
+			if(payUtil!=null){
+				payUtil.onActivityResult(requestCode, resultCode, data);
+			}
+		}
+		
+		if(payselect == EPPayHelper.Pay_WXPay){
+			if(wxPayUtil!=null){
+				wxPayUtil.onActivityResult(requestCode, resultCode, data);
+			}
+		}
+	}
+	
+	
 }
---
+++ ThirdPay_SmsPay/helper_noin_third_sms_shuicg/src/com/epplus/utils/WXPayUtil.java
@@ -0,0 +1,212 @@
+package com.epplus.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import com.ipaynow.plugin.api.IpaynowPlugin;
+import com.xqt.now.paysdk.XqtPay;
+import com.xqt.now.paysdk.XqtPay.XqtPayListener;
+
+/**
+ * 微信支付
+ * @author zgt
+ *
+ */
+public class WXPayUtil implements XqtPayListener{
+
+	private  Activity act = null;
+	private static ProgressDialog progressDialog = null;
+	
+	// 商户秘钥
+	private static final String key = "88c1a59b8fa9d217c8c632c2921ef286";
+	
+	private  String notifyUrl;
+	
+	private WXPayHandler wxpayHandler;
+	
+	public WXPayUtil(Activity act,WXPayHandler wxpayHandler) {
+		this.act =act;
+		this.wxpayHandler = wxpayHandler;
+		
+		String baseUrl = "http://thirdpay-webhook.n8wan.com:29141/thirdpayCountServlet";
+		this.notifyUrl = baseUrl+"?"+ConfigUtils.xx_notifyData+"="+ConfigUtils.getNotifyJsonData(act,ConfigUtils.WX);
+	}
+	
+	
+	/**
+	 * 支付
+	 * @param price  商品金额 （分）
+	 * @param orderName   商品名称
+	 * @param orderDetail  商品描述
+	 */
+	public  void pay(String price,String orderName,String orderDetail){
+		prePayMessage(price, orderName, orderDetail);
+		XqtPay.mhtOrderNo = new SimpleDateFormat("yyyyMMddHHmmss",
+				Locale.CHINA).format(new Date());
+		XqtPay.payChannelType = "13";
+		XqtPay.sign = Sign();
+		goToPay();
+		
+		
+	}
+	
+	
+	private void goToPay() {
+		ConnectivityManager manager = (ConnectivityManager)act.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = manager.getActiveNetworkInfo();
+		if (info != null && info.isConnected()) {
+			progressDialog = new ProgressDialog(act);
+			progressDialog.setTitle("进度提示");
+			progressDialog.setMessage("支付安全环境扫描");
+			progressDialog.setCancelable(false);
+			progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			progressDialog.show();
+			// 获取支付参数
+			XqtPay.Transit(act, this);
+
+		} else {
+			Builder builder = new AlertDialog.Builder(act);
+			builder.setTitle("网络状态");
+			builder.setMessage("没有可用网络,是否进入设置面板");
+			builder.setPositiveButton("是",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							act.startActivity(new Intent(
+									android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+						}
+					});
+			builder.setNegativeButton("否",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							Toast.makeText(act, "联网失败", Toast.LENGTH_SHORT)
+									.show();
+						}
+					});
+			builder.create().show();
+		}
+	}
+	
+	
+	
+	@Override
+	public void success(String str) {
+		progressDialog.dismiss();
+		// 是否打开未支付返回再次支付提示
+		IpaynowPlugin.setShowConfirmDialog(true);
+		// 发起支付请求
+		IpaynowPlugin.pay(act, str);
+		
+	}
+	
+	
+	@Override
+	public void error(String str) {
+		progressDialog.dismiss();
+		//Toast.makeText(act, str, 1).show();
+		
+	}
+
+	
+	
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (data == null) {
+			return;
+		}
+		String respCode = data.getExtras().getString("respCode");
+		String respMsg = data.getExtras().getString("respMsg");
+		
+		StringBuilder temp = new StringBuilder();
+		if (respCode.equals("00")) {
+			temp.append("交易状态:成功");
+			wxpayHandler.WXPaySuccess(temp.toString(), "");
+		}
+
+		if (respCode.equals("02")) {
+			temp.append("交易状态:取消");
+			wxpayHandler.WXPayCancel(temp.toString(), "");
+		}
+
+		if (respCode.equals("01")) {
+			temp.append("交易状态:失败").append("\n").append("原因:" + respMsg);
+			wxpayHandler.WXPayFailed(temp.toString(), "");
+		}
+
+		if (respCode.equals("03")) {
+			temp.append("交易状态:未知").append("\n").append("原因:" + respMsg);
+		}
+		
+	}
+
+
+	
+	
+	
+	
+	private void prePayMessage(String price,String orderName,String orderDetail) {
+		
+		XqtPay.consumerId = "154345";
+		XqtPay.mhtOrderName = orderName;
+		XqtPay.mhtOrderAmt = price;
+		XqtPay.mhtOrderDetail = orderDetail;
+		XqtPay.notifyUrl =notifyUrl;
+		XqtPay.superid = "100000";
+		
+		IpaynowPlugin.setShowConfirmDialog(false);
+	}
+	
+	
+	private String Sign() {
+		String str = "customerid=" + XqtPay.consumerId + "&sdcustomno="
+				+ XqtPay.mhtOrderNo + "&orderAmount=" + XqtPay.mhtOrderAmt
+				+ key;
+		return getMD5(str).toUpperCase();
+	}
+	
+	public static String getMD5(String content) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			digest.update(content.getBytes());
+			return getHashString(digest);
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private static String getHashString(MessageDigest digest) {
+		StringBuilder builder = new StringBuilder();
+		for (byte b : digest.digest()) {
+			builder.append(Integer.toHexString((b >> 4) & 0xf));
+			builder.append(Integer.toHexString(b & 0xf));
+		}
+		return builder.toString();
+	}
+
+
+	public static abstract class WXPayHandler{
+		public abstract void WXPaySuccess(String resultInfo,String resultStatus);
+		public abstract void WXPayFailed(String resultInfo,String resultStatus);
+		public abstract void WXPayCancel(String resultInfo, String resultStatus);
+	}
+
+	
+	
+}
