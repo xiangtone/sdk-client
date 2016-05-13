30c72c8ee7c7adce3d6417d01e4657d828bce48e
zhangguangtao <zgt1207834821@163.com>
2016-05-13 09:49:26 +0800

    1.2

---
+++ ThirdPay_SmsPay1.2/ThirdPay_SmsPay1.2/src/com/epplus/statistics/StatisURL.java
@@ -0,0 +1,14 @@
+package com.epplus.statistics;
+/**
+ * Í³¼ÆµÄurl
+ * @author zgt
+ *
+ */
+public class StatisURL {
+	
+	
+	//public  static final String BASEURL = "http://192.168.0.101:8080/thirdpay-webhook/PayOperateCountServlet";
+	//public  static final String BASEURL = "http://thirdpay-webhook.n8wan.com:29141/PayOperateCountServlet";
+	
+
+}
---
+++ ThirdPay_SmsPay1.2/ThirdPay_SmsPay1.2/src/com/epplus/utils/LLog.java
@@ -0,0 +1,12 @@
+package com.epplus.utils;
+
+import android.util.Log;
+
+public class LLog {
+public static void log(String log) {
+Log.d("ep", log);
+}
+public static void info(String log) {
+	Log.i("ep", log);	
+}
+}
