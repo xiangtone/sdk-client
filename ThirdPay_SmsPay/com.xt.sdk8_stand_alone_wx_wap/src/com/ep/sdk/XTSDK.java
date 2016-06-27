530cf5e248628171068dc00c6599c57c6c437b61
ghosted5 <2221817588@qq.com>
2016-06-27 18:25:26 +0800

    增加支付宝和微信wap的demo工程

---
+++ ThirdPay_SmsPay/com.xt.sdk8_stand_alone_wx_wap/src/com/ep/sdk/XTSDK.java
@@ -0,0 +1,78 @@
+package com.ep.sdk;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import com.epplus.publics.EPPayHelper;
+import com.epplus.view.PayParams;
+
+public class XTSDK {
+	
+	private static XTSDK xtsdk;
+	
+	private boolean isInit;
+	
+	
+	private XTSDK(){
+		isInit = false;
+	} 
+	
+	/**
+	 * 获取XTSDK
+	 * @return
+	 */
+	public static XTSDK getInstance(){
+		if(xtsdk==null){
+			xtsdk = new XTSDK();
+		}
+		return xtsdk;
+	}
+	
+	/**
+	 * 初始化
+	 */
+	public void init(final Activity ac,String payContact,Handler handler){
+		EPPayHelper.getInstance(ac).initPay(true,payContact);//"4001059566"
+		EPPayHelper.getInstance(ac).setPayListen(handler);		
+		isInit = true;
+		
+	}
+	
+	
+	/**
+	 * 支付 
+	 */
+	public boolean pay(Activity ac,PayParams params){
+		
+		if (isInit) {
+			EPPayHelper.getInstance(ac).pay(params);
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * 退出
+	 */
+	public void exit(Activity ac){
+		try {
+			EPPayHelper.getInstance(ac).exit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * 支付回调结果
+	 * @param requestCode
+	 * @param resultCode
+	 * @param data
+	 */
+	public void payCallResult(Activity ac,int requestCode, int resultCode, Intent data){
+		EPPayHelper.getInstance(ac).onActivityResult(requestCode, resultCode, data);
+	}
+	
+
+}
